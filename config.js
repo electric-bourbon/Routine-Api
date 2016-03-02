1e86de02ef260804abf17ffb80fe4ab135f5f26c
Nick Leach <nickleach22@gmail.com>
2016-03-02 17:50:11 -0500

    fixed stuff

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'port' : process.env.PORT || 8080,
+  'port' : process.env.PORT || 7001,
   'database' : 'mongodb://nick:nick@ds019638.mlab.com:19638/routine-test',
   'secret': 'thisisasecretyoyoyo',
   'company' : 'Matrix'
