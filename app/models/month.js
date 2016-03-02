c52d1a26e58cc4c60a751094436c877cf56b004a
Nick Leach <nickleach22@gmail.com>
2016-03-01 23:44:13 -0500

    initial commit

---
+++ app/models/month.js
@@ -0,0 +1,18 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var MonthSchema  = new Schema({
+  month: Number,
+  year: Number,
+  days: [
+    {
+      day: Number,
+      percentageComplete: Number,
+    }
+  ],
+  factorableDays: Number,
+  routineId: String,
+  userId: String
+});
+
+module.exports = mongoose.model('Month', MonthSchema);
---
+++ app/models/subRoutine.js
@@ -0,0 +1,14 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var SubRoutineSchema  = new Schema({
+  name: String,
+  status: Number,
+  desiredFrequency: Number,
+  currentCount: Number,
+  completed: Boolean,
+  routineId: String,
+  userId: String
+});
+
+module.exports = mongoose.model('SubRoutine', SubRoutineSchema);
